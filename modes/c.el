1e0111646e94e4897edd4cd6b9856090fb2a1911
Joshua Peek <josh@joshpeek.com>
2009-01-23 00:02:06 -0600

    switch to 8 width tabs

--- modes/c.el
+++ modes/c.el
@@ -2,7 +2,7 @@
 (add-hook 'c-mode-hook
           '(lambda ()
              (c-set-style "k&r")
-             (setq c-basic-offset 4)
-             (setq tab-width 4)
+             (setq c-basic-offset 8)
+             (setq tab-width 8)
              (setq indent-tabs-mode t)
              (add-hook 'before-save-hook 'tabify-buffer)))
