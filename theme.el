1ce4249ecc99a56d43429462fcbfd58549485cbe
Joshua Peek <josh@joshpeek.com>
2009-02-07 22:24:19 -0600

    change default font to monaco 18

--- theme.el
+++ theme.el
@@ -17,10 +17,6 @@
 	(width . 237) (height . 65)))
 
 ; font
-(if system-macbook-pro
-    (setq default-frame-alist '((font . "-apple-Bitstream_Vera_Sans_Mono-medium-normal-normal-Roman-18-*-*-*-*-*-fontset-auto7"))))
-
-(if system-macbook-air
-  (setq default-frame-alist '((font . "-apple-Bitstream_Vera_Sans_Mono-medium-normal-normal-Roman-14-*-*-*-*-*-fontset-auto7"))))
+(setq default-frame-alist '((font . "-apple-Monaco-medium-normal-normal--18-*-*-*-*-*-fontset-auto1")))
 
 (provide 'theme)
