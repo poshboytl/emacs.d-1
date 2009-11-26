33fc7772467581ca4b1a0fb57fd710d35eb4352b
Joshua Peek <josh@joshpeek.com>
2009-11-26 17:37:31 -0600

    add rackup and gemfile to ruby mode

--- modes.el
+++ modes.el
@@ -53,6 +53,8 @@
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 
 (font-lock-add-keywords
       'ruby-mode
