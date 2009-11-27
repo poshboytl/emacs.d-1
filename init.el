8985a23b8f8c8a8e8604a4afd171c494cf7db400
Joshua Peek <josh@joshpeek.com>
2009-11-26 22:17:32 -0600

    setup proper autoloading

--- init.el
+++ init.el
@@ -4,6 +4,10 @@
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
 (add-to-list 'load-path dotfiles-dir)
+(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
+
+(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
+(setq package-user-dir (concat dotfiles-dir "elpa"))
 (setq custom-file (concat dotfiles-dir "custom.el"))
 (setq vendor-dir (concat dotfiles-dir "vendor"))
 (add-to-list 'load-path vendor-dir)
@@ -20,6 +24,7 @@
 (vendor 'textmate)
 (textmate-mode)
 
+(regen-autoloads)
 (load custom-file 'noerror)
 
 (provide 'init)
