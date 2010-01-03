9f1f57ed3f2586d91dc4910eb33a383eca220141
Joshua Peek <josh@joshpeek.com>
2010-01-03 12:19:33 -0600

    split out buffers file and enable uniquify

--- init.el
+++ init.el
@@ -13,6 +13,7 @@
 (require 'global)
 (require 'defuns)
 (require 'bindings)
+(require 'buffers)
 (require 'modes)
 (require 'theme)
 
--- modes.el
+++ modes.el
@@ -1,11 +1,3 @@
-;; ido
-(ido-mode t)
-(setq ido-enable-prefix nil
-      ido-enable-flex-matching t
-      ido-create-new-buffer 'always
-      ido-use-filename-at-point t
-      ido-max-prospects 10)
-
 ;; C
 (add-hook 'c-mode-hook
           '(lambda ()
