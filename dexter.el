f1d1793af926377b34c5ac763ea1a3f495d7ab33
Joshua Peek <josh@joshpeek.com>
2010-01-05 11:08:32 -0600

    set initial position in local config

---
+++ dexter.el
@@ -0,0 +1,4 @@
+; window
+(setq default-frame-alist
+      '((left . 40) (top . 60)
+	(width . 120) (height . 55)))
---
+++ scout.el
@@ -0,0 +1,4 @@
+;; window
+(setq default-frame-alist
+      '((left . 20) (top . 40)
+ 	(width . 80) (height . 45)))
