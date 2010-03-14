3e8a898af8f6d55b3d9fc8bd9b7fadc31cc9c7f6
Joshua Peek <josh@joshpeek.com>
2010-03-14 13:34:24 -0500

    move vendored package initialization into elpa.el

--- elpa.el
+++ elpa.el
@@ -1,5 +1,8 @@
-(require 'package)
-(package-initialize)
+(setq package-user-dir (concat dotfiles-dir "elpa"))
+(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
+(setq vendor-dir (concat dotfiles-dir "elpa-to-submit"))
+
+(add-to-list 'load-path vendor-dir)
 
 (defvar packages (list
 		  'full-ack
@@ -20,5 +23,23 @@
 		(functionp package))
       (message "Installing %s" (symbol-name package))
       (package-install package))))
+
+(defun regen-autoloads (&optional force-regen)
+  "Regenerate the autoload definitions file if necessary and load it."
+  (interactive "P")
+  (let ((autoload-dir vendor-dir)
+        (generated-autoload-file autoload-file))
+    (when (or force-regen
+              (not (file-exists-p autoload-file))
+              (some (lambda (f) (file-newer-than-file-p f autoload-file))
+                    (directory-files autoload-dir t "\\.el$")))
+      (message "Updating autoloads...")
+      (let (emacs-lisp-mode-hook)
+        (update-directory-autoloads autoload-dir))))
+  (load autoload-file))
+
+(require 'package)
+(package-initialize)
+(regen-autoloads)
 
 (provide 'elpa)
--- init.el
+++ init.el
@@ -2,10 +2,7 @@
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
 (add-to-list 'load-path dotfiles-dir)
-(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
 
-(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
-(setq package-user-dir (concat dotfiles-dir "elpa"))
 (setq custom-file (concat dotfiles-dir "custom.el"))
 
 (require 'cl)
@@ -17,7 +14,6 @@
 (require 'modes)
 (require 'theme)
 
-(regen-autoloads)
 (load custom-file 'noerror)
 
 ;; system specific config
