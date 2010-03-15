78b4b7c287ccf8b93f7fd98cae5eb4053ee85215
Joshua Peek <josh@joshpeek.com>
2010-03-14 23:13:46 -0500

    don't need css mode load def either

--- modes.el
+++ modes.el
@@ -11,9 +11,6 @@
              (add-hook 'before-save-hook 'tabify-buffer)))
 
 ;; CSS
-(autoload 'css-mode "css-mode" "Mode for editing CSS file" t)
-(add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
-
 (add-hook 'css-mode-hook
           '(lambda ()
              (setq css-indent-offset 2)
