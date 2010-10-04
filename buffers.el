c4b1bcb2bd7ce7bd418983d7797387f8603d0f85
Joshua Peek <josh@joshpeek.com>
2010-10-03 22:14:26 -0500

    Don't use filename at point

--- buffers.el
+++ buffers.el
@@ -2,7 +2,6 @@
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
       ido-create-new-buffer 'always
-      ido-use-filename-at-point t
       ido-max-prospects 10)
 
 ;; nicer buffer names
