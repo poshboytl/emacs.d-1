1e67caed40faf46ae4ab29a890d482985302361d
Joshua Peek <josh@joshpeek.com>
2010-12-14 00:00:03 -0600

    Preserve window with when speedbar is toggled

--- buffers.el
+++ buffers.el
@@ -28,4 +28,15 @@
 (setq sr-speedbar-right-side nil)
 (setq sr-speedbar-width-console 40)
 
+(defadvice sr-speedbar-open (after sr-speedbar-open-resize-frame activate)
+  (set-frame-width (selected-frame)
+		   (+ (frame-width) sr-speedbar-width)))
+(ad-enable-advice 'sr-speedbar-open 'after 'sr-speedbar-open-resize-frame)
+
+(defadvice sr-speedbar-close (after sr-speedbar-close-resize-frame activate)
+  (sr-speedbar-recalculate-width)
+  (set-frame-width (selected-frame)
+		   (- (frame-width) sr-speedbar-width)))
+(ad-enable-advice 'sr-speedbar-close 'after 'sr-speedbar-close-resize-frame)
+
 (provide 'buffers)
