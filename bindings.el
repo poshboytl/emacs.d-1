9c9a122a17da0b8bf7dc6417ab582eee915f6f1a
Joshua Peek <josh@joshpeek.com>
2011-01-11 18:10:18 -0600

    Kill some unused bindings

--- bindings.el
+++ bindings.el
@@ -1,22 +1,15 @@
-;; buffer menu
-(global-set-key (kbd "C-x C-b") 'buffer-menu)
-
 ;; speedbar
 (global-set-key (kbd "C-c s") 'sr-speedbar-toggle)
 
-(if (boundp 'osx-key-mode-map)
-    (progn
-      (define-key osx-key-mode-map (kbd "A-w") 'close-buffer)
-      (define-key osx-key-mode-map (kbd "A-k") 'kill-this-buffer)))
+;; window
+(global-set-key (kbd "s-w") 'close-buffer)
+(global-set-key (kbd "s-k") 'kill-this-buffer)
 
 ;; font size
 (global-set-key (kbd "s-+") 'text-scale-adjust)
 (global-set-key (kbd "s--") 'text-scale-adjust)
 (global-set-key (kbd "s-0") 'text-scale-adjust)
 (global-set-key (kbd "s-=") 'text-scale-adjust)
-
-;; prefer dired over dumping dir list to buffer
-(global-set-key "\C-x\C-d" 'dired)
 
 ;; M-x without meta
 (global-set-key (kbd "C-x C-m") 'execute-extended-command)
