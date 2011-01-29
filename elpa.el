325888a7b2d5141bd66b4ddaf88ba2229171302f
Joshua Peek <josh@joshpeek.com>
2011-01-29 15:09:00 -0600

    Remove vendor dir

--- elpa.el
+++ elpa.el
@@ -1,8 +1,5 @@
 (setq package-user-dir (concat dotfiles-dir "elpa"))
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
-(setq vendor-dir (concat dotfiles-dir "elpa-to-submit"))
-
-(add-to-list 'load-path vendor-dir)
 
 (defvar packages (list
                   'coffee-mode
@@ -39,26 +36,11 @@
       (message "Installing %s" (symbol-name package))
       (package-install package))))
 
-(defun regen-autoloads (&optional force-regen)
-  "Regenerate the autoload definitions file if necessary and load it."
-  (interactive "P")
-  (let ((autoload-dir vendor-dir)
-	(generated-autoload-file autoload-file))
-    (when (or force-regen
-              (not (file-exists-p autoload-file))
-              (some (lambda (f) (file-newer-than-file-p f autoload-file))
-                    (directory-files autoload-dir t "\\.el$")))
-      (message "Updating autoloads...")
-      (let (emacs-lisp-mode-hook)
-	(update-directory-autoloads autoload-dir))))
-  (load autoload-file))
-
 (require 'package)
 (dolist (source '(("josh" . "http://josh.github.com/elpa/")
                   ("technomancy" . "http://repo.technomancy.us/emacs/")
                   ("elpa" . "http://tromey.com/elpa/")))
   (add-to-list 'package-archives source t))
 (package-initialize)
-(regen-autoloads)
 
 (provide 'elpa)
