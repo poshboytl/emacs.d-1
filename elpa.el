2b6d62f37793a311ee2212c544b586a4334d236d
Joshua Peek <josh@joshpeek.com>
2011-12-23 17:47:13 -0600

    Cleanup elpa stuff

--- elpa.el
+++ elpa.el
@@ -1,41 +1,37 @@
-(setq package-user-dir (concat dotfiles-dir "elpa"))
+(require 'package)
 
-(defvar packages (list
-                  'coffee-mode
-                  'color-theme
-                  'color-theme-solarized
-                  'color-theme-twilight
-                  'erlang
-                  'full-ack
-                  'haskell-mode
-                  'inf-ruby
-                  'magit
-                  'mustache-mode
-                  'peepopen
-                  'rhtml
-                  'ruby-mode
-                  'starter-kit
-                  'starter-kit-bindings
-                  'starter-kit-lisp
-                  'starter-kit-ruby
-                  'textmate)
+(setq package-archives '(("josh" . "http://josh.github.com/elpa/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("elpa" . "http://tromey.com/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar packages '(coffee-mode
+                   color-theme
+                   color-theme-solarized
+                   color-theme-twilight
+                   erlang
+                   full-ack
+                   haskell-mode
+                   inf-ruby
+                   magit
+                   mustache-mode
+                   peepopen
+                   rhtml
+                   ruby-mode
+                   starter-kit
+                   starter-kit-bindings
+                   starter-kit-lisp
+                   starter-kit-ruby
+                   textmate)
   "Libraries that should be installed by default.")
 
-(defun elpa-install ()
-  "Install all starter-kit packages that aren't installed."
-  (interactive)
-  (package-refresh-contents)
-  (dolist (package packages)
-    (unless (or (member package package-activated-list)
-                (functionp package))
-      (message "Installing %s" (symbol-name package))
-      (package-install package))))
-
-(require 'package)
-(dolist (source '(("josh" . "http://josh.github.com/elpa/")
-                  ("marmalade" . "http://marmalade-repo.org/packages/")
-                  ("elpa" . "http://tromey.com/elpa/")))
-  (add-to-list 'package-archives source t))
-(package-initialize)
+(dolist (package packages)
+  (when (not (package-installed-p package))
+    (package-install package)))
 
 (provide 'elpa)
